b37e58908c2be269f3c643c4c9ab9e998b39841d
ItsJESH <jenishpaghadalin018@gmail.com>
2025-01-16 10:42:01 +0530

    Added Test Page v0.0.1

---
+++ lr.js
@@ -0,0 +1,29 @@
+document.getElementById("rgcon").style.display = "none";
+const stylesheet = document.getElementById("rgcss");
+stylesheet.parentNode.removeChild(stylesheet);
+
+
+document.getElementById("maincon").addEventListener("click", (event) => {
+  if (event.target.id === "sup") {
+    const link = document.createElement("link");
+    link.id = "rgcss";
+    link.rel = "stylesheet";
+    link.href = "./rgcss.css";
+    document.head.appendChild(link);
+    document.getElementById("rgcon").style.display = "block";
+    document.getElementById("lgcon").style.display = "none";
+    const stylesheet = document.getElementById("lgcss");
+    stylesheet.parentNode.removeChild(stylesheet);
+
+} else if (event.target.id === "sin") {
+  const link = document.createElement("link");
+  link.id = "lgcss";
+  link.rel = "stylesheet";
+  link.href = "./lgcss.css";
+  document.head.appendChild(link);
+  document.getElementById("lgcon").style.display = "block";
+  document.getElementById("rgcon").style.display = "none";
+  const stylesheet = document.getElementById("rgcss");
+  stylesheet.parentNode.removeChild(stylesheet);
+  }
+});
